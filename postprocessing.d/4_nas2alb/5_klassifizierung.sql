54f110b4e4217184cf7c95a03ce491ad09371615
ARCHIKART Software AG <lni@archikart.de>
2023-08-28 18:23:24 +0200

    Fix >> Fehler in Script

--- postprocessing.d/4_nas2alb/5_klassifizierung.sql
+++ postprocessing.d/4_nas2alb/5_klassifizierung.sql
@@ -19,7 +19,7 @@ INSERT INTO alkis_klassifizierungen(name, kennung, prefix, funktionsfeld, bodenz
 	('ax_bodenschaetzung',			'72001', 'b', 'nutzungsart',		'bodenzahlodergruenlandgrundzahl',	'ackerzahlodergruenlandzahl',	'ax_nutzungsart_bodenschaetzung'),
 	('ax_bewertung',			'72004', 'B', 'klassifizierung',	'NULL::varchar',			'NULL::varchar',		'ax_klassifizierung_bewertung'),
 	('ax_klassifizierungnachwasserrecht',	'71003', 'W', 'artderfestlegung',	'NULL::varchar',			'NULL::varchar',		'ax_artderfestlegung_klassifizierungnachwasserrecht'),
-	('ax_klassifizierungnachstrassenrecht',	'71001', 'S', 'artderfestlegung',	'NULL::varchar',			'NULL::varchar',		'ax_artderfestlegung_klassifizierungnachstrassenrecht');
+	('ax_klassifizierungnachstrassenrecht',	'71001', 'S', 'artderfestlegung',	'NULL::varchar',			'NULL::varchar',		'ax_artderfestlegung_klassifizierungnachstrassenrecht'),
 	('ax_musterlandesmusterundvergleichsstueck', '72002', 'M', 'kulturart',		'bodenzahlodergruenlandgrundzahl',	'ackerzahlodergruenlandzahl',	'ax_kulturart_musterlandesmusterundvergleichsstueck'),
 	('ax_anderefestlegungnachwasserrecht', '71004', 'W1', 'artderfestlegung',	'NULL::varchar',			'NULL::varchar',		'ax_artderfestlegung_anderefestlegungnachwasserrecht'),
 	('ax_anderefestlegungnachstrassenrecht', '71002', 'S1', 'artderfestlegung',	'NULL::varchar',			'NULL::varchar',		'ax_artderfestlegung_anderefestlegungnachstrassenrecht'),
