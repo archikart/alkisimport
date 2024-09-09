793b479caf105caf20792fe5d7c9fe92dedd8a6d
ARCHIKART Software AG <fsa@archikart.de>
2024-09-09 19:50:09 +0200

    Stored Procedures Postgis
    - Ermittlung Flächen, Geoemetrien, Buffer-Test für Geometriesaum von Füllflächen für Splissflächenverarbeitung

--- alkis-archikart.sql
+++ alkis-archikart.sql
@@ -34,3 +34,111 @@ CREATE INDEX idx_alkis_insert_typename
 ON alkis_insert (typename);
 CREATE INDEX idx_alkis_insert_featureid 
 ON alkis_insert (featureid);
+
+CREATE OR REPLACE function alkis_fs_fuellfl_geom(fs_id varchar, join_table varchar) RETURNS geometry
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  fs_ff_geom geometry;
+BEGIN
+  EXECUTE format('SELECT st_difference(max(f.wkb_geometry),st_union(st_intersection(f.wkb_geometry,j.wkb_geometry)))
+                  FROM ax_flurstueck f
+                  JOIN %I j
+                  ON f.wkb_geometry && j.wkb_geometry
+                  AND st_relate(f.wkb_geometry,j.wkb_geometry,%L)
+                  WHERE f.endet IS NULL
+                  AND f.gml_id = %L',
+                 join_table,'2********',fs_id)
+  INTO fs_ff_geom;
+  
+  RETURN fs_ff_geom;
+END;
+$$ SET search_path TO archikart, public;
+
+CREATE OR REPLACE function alkis_fs_fuellfl_umfang(fs_id varchar, join_table varchar) RETURNS float
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  fs_ff_umfang float;
+BEGIN
+  EXECUTE format('SELECT st_perimeter2d(st_difference(max(f.wkb_geometry),st_union(st_intersection(f.wkb_geometry,j.wkb_geometry))))
+                  FROM ax_flurstueck f
+                  JOIN %I j
+                  ON f.wkb_geometry && j.wkb_geometry
+                  AND st_relate(f.wkb_geometry,j.wkb_geometry,%L)
+                  WHERE f.endet IS NULL
+                  AND f.gml_id = %L',
+                 join_table,'2********',fs_id)
+  INTO fs_ff_umfang;
+  
+  RETURN fs_ff_umfang;
+END;
+$$ SET search_path TO archikart, public;
+
+CREATE OR REPLACE function alkis_fs_ab_geom(fs_id varchar, ab_id varchar, join_table varchar) RETURNS geometry
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  ab_geom geometry;
+BEGIN
+  EXECUTE format('SELECT st_intersection(f.wkb_geometry,j.wkb_geometry)
+                  FROM ax_flurstueck f
+                  JOIN %I j
+                  ON f.wkb_geometry && j.wkb_geometry
+                  AND st_relate(f.wkb_geometry,j.wkb_geometry,%L)
+                  WHERE f.endet IS NULL
+                  AND f.gml_id = %L
+                  AND j.gml_id = %L',
+                 join_table,'2********',fs_id,ab_id)
+  INTO ab_geom;
+  
+  RETURN ab_geom;
+END;
+$$ SET search_path TO archikart, public;
+
+CREATE OR REPLACE function alkis_buffer_test(geom geometry, buffer_offset float) RETURNS boolean
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  is_empty boolean;
+BEGIN
+  SELECT st_isempty(st_buffer(geom,-buffer_offset))
+  INTO is_empty;
+  
+  RETURN is_empty;
+END;
+$$ SET search_path TO archikart, public;
+
+CREATE OR REPLACE function alkis_fs_fuellfl_buffer_test(fs_id varchar, join_table varchar, buffer_offset float) RETURNS boolean
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  is_empty boolean;
+BEGIN
+  EXECUTE format('SELECT alkis_buffer_test(alkis_fs_fuellfl_geom(%L,%L),%s)',
+                 fs_id,join_table,buffer_offset)
+  INTO is_empty;
+  
+  RETURN is_empty;
+END;
+$$ SET search_path TO archikart, public;
+
+CREATE OR REPLACE function alkis_fs_ab_buffer_test(fs_id varchar, ab_id varchar, join_table varchar, buffer_offset float) RETURNS boolean
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  is_empty boolean;
+BEGIN
+  EXECUTE format('SELECT alkis_buffer_test(alkis_fs_ab_geom(%L,%L,%L),%s)',
+                 fs_id,ab_id,join_table,buffer_offset)
+  INTO is_empty;
+  
+  RETURN is_empty;
+END;
+$$ SET search_path TO archikart, public;
