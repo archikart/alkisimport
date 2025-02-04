c1f5d960a4e9d59259a9dfe5d0698fbd34e32fe3
ARCHIKART Software AG <fsa@archikart.de>
2025-02-04 16:02:39 +0100

    Erweiterung Verarbeitung Füllflächen

--- postcreate.d/archikart.sql
+++ postcreate.d/archikart.sql
@@ -1,23 +1,31 @@
-\unset ON_ERROR_STOP
-SET client_min_messages TO notice;
-\set ON_ERROR_STOP
-
-SET search_path = :"alkis_schema", :"postgis_schema", public;
+SET search_path = :"alkis_schema", public;
 
 SELECT alkis_dropobject('ak_fs_fuellfl_umfang_klass');
 CREATE TABLE ak_fs_fuellfl_umfang_klass (
 	fs_gml_id character(16),
-	fuellfl_umfang double precision,	
+	fuellfl_umfang double precision,
+        wkb_geometry geometry,	
 	primary key (fs_gml_id)
 );
-COMMENT ON TABLE ak_fs_fuellfl_umfang_klass IS 'ARCHIKART: Füllflächenumfang Flurstücke';
+COMMENT ON TABLE ak_fs_fuellfl_umfang_klass IS 'ARCHIKART: Füllflächenumfang Flurstücke - K0003AXAbschnittKF';
+CREATE INDEX ak_fs_fuellfl_umfang_klass_idx1 ON ak_fs_fuellfl_umfang_klass(fs_gml_id);
+
+SELECT alkis_dropobject('ak_fs_fuellfl_umfang_nutz_klass');
+CREATE TABLE ak_fs_fuellfl_umfang_nutz_klass (
+	fs_gml_id character(16),
+	fuellfl_umfang double precision,
+        wkb_geometry geometry,	
+	primary key (fs_gml_id)
+);
+COMMENT ON TABLE ak_fs_fuellfl_umfang_nutz_klass IS 'ARCHIKART: Füllflächenumfang Flurstücke - K0003AXAbschnittNK';
 CREATE INDEX ak_fs_fuellfl_umfang_klass_idx1 ON ak_fs_fuellfl_umfang_klass(fs_gml_id);
 
 SELECT alkis_dropobject('ak_fs_fuellfl_umfang_nutz');
 CREATE TABLE ak_fs_fuellfl_umfang_nutz(
 	fs_gml_id character(16),
-	fuellfl_umfang double precision,	
+	fuellfl_umfang double precision,
+        wkb_geometry geometry,		
 	primary key (fs_gml_id)
 );
-COMMENT ON TABLE ak_fs_fuellfl_umfang_nutz IS 'ARCHIKART: Füllflächenumfang Flurstücke Nutzung';
+COMMENT ON TABLE ak_fs_fuellfl_umfang_nutz IS 'ARCHIKART: Füllflächenumfang Flurstücke - K0003AXAbschnitt';
 CREATE INDEX ak_fs_fuellfl_umfang_nutz_idx1 ON ak_fs_fuellfl_umfang_nutz(fs_gml_id);
